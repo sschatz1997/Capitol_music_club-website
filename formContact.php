d592925539f454c1df38f7bbc962c35b35151e25
sschatz1997 <s.schatz1997@gmail.com>
2018-06-12 13:39:21 -0400

    added visitor log

--- formContact.php
+++ formContact.php
@@ -4,9 +4,11 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 
 $file = fopen("contact.txt", "w+") or die ("file not open");
-
 $f = $name.",".$email."\n";
 fputs($file,$f) or die ("no data was written");
+
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
 
 fclose($file);
 header("Location: http://10.0.54.169/Redirect.html"); 
