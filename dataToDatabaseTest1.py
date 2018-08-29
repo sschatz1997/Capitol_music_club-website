9bfac016449afa57dd1de77374f1c7d149128d86
sschatz1997 <s.schatz1997@gmail.com>
2018-08-28 22:16:10 -0400

    w

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -1,12 +1,14 @@
 from time import sleep as s
+import sqlite as s
 
+c = s.connect('test.db')
 contactsMain = "/var/www/html/contact.txt"
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
 
 with open(contactsMain) as f:
 	while 0 != 10:
-		line = readlines(array[i])
+		line = f.readlines(array[i])
 		print(line)
 		s(.1)
 		i += 1
