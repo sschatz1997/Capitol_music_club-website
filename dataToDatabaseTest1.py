562f810aba5e38edf29fa5faf28b046b2dfda803
sschatz1997 <s.schatz1997@gmail.com>
2018-08-29 10:11:56 -0400

    formatting

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -2,7 +2,7 @@ from time import sleep
 import sqlite3 as s
 
 c = s.connect('test.db')
-cur = con.cursor()
+cur = c.cursor()
 contactsMain = "/var/www/html/contact.txt"
 database = "/root/disk3/database.db"
 table1 = 'table1'
@@ -16,7 +16,7 @@ with open(contactsMain, "r") as f:
 		line = f.readlines(array[1])
 		print(line)
 		try:
-			cin.excute("INSERT INTO {tn} {cn}VALUES (123456, 'test')".\
+			c.excute("INSERT INTO {tn} {cn}VALUES (123456, 'test')".\
 				format(tn=table_name, idf=id_column, cn=column_name))
 		except s.IntegrityError:
 			print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
