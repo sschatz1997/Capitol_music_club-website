b1b2e50c0728433d03dfa9b8695f228d7363520a
sschatz1997 <s.schatz1997@gmail.com>
2018-08-29 10:09:24 -0400

    sqlite stuff

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -1,18 +1,27 @@
 from time import sleep 
 import sqlite3 as s
 
-#c = s.connect('test.db')
+c = s.connect('test.db')
+cur = con.cursor()
 contactsMain = "/var/www/html/contact.txt"
 database = "/root/disk3/database.db"
+table1 = 'table1'
+column1 = 'column1'
 
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
 
-with open(contactsMain) as f:
+with open(contactsMain, ") as f:
 	while True:
 		line = f.readlines(array[1])
-		splitdata = line.split(";")
-		print(splitdata)
+		print(line)
+			try:
+				cin.excute("INSERT INTO {tn} {cn}VALUES (123456, 'test')".\
+					format(tn=table_name, idf=id_column, cn=column_name))
+			except s.IntegrityError:
+				print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+		c.commit()
 		sleep(.1)
-		#del(line)
+		del(line)
+		c.close()
 f.close()
