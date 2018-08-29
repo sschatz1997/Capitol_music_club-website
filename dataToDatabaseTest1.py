a93ff5ce7a916caed37c2bbcb3e91de1850c3453
sschatz1997 <s.schatz1997@gmail.com>
2018-08-28 21:01:50 -0400

    tests

---
+++ dataToDatabaseTest1.py
@@ -0,0 +1,13 @@
+from time import sleep as s
+
+contactsMain = "/var/www/html/contact.txt"
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+i = 0
+
+with open as f:
+	while 0 != 10:
+		line = readline(array[i])
+		print(line)
+		s(.1)
+		i += 1
+f.close()
