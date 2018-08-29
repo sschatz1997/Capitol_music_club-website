e6c557e1796c1bcd080522a539333de09ddc7e5e
sschatz1997 <s.schatz1997@gmail.com>
2018-08-28 22:26:48 -0400

    database entry

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -3,13 +3,15 @@ import sqlite3 as s
 
 c = s.connect('test.db')
 contactsMain = "/var/www/html/contact.txt"
+database = "/root/disk3/database.db"
+
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
 
 with open(contactsMain) as f:
-	while 0 != 10:
-		line = f.readlines(array[i])
+	while True:
+		line = f.readlines(array[:1])
 		print(line)
 		sleep(.1)
-		i += 1
+		del(line)
 f.close()
