0ceb212381d98d8376a1c846a34e314e6eb1abdf
sschatz1997 <s.schatz1997@gmail.com>
2018-08-28 21:06:06 -0400

    fix

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -4,7 +4,7 @@ contactsMain = "/var/www/html/contact.txt"
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
 
-with open as f:
+with open(contactsMain) as f:
 	while 0 != 10:
 		line = readline(array[i])
 		print(line)
