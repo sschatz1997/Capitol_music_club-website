44ccc3dd3a9e37a0de8c3917745992b3432513f8
sschatz1997 <s.schatz1997@gmail.com>
2018-08-29 10:14:32 -0400

    added second column

--- dataToDatabaseTest1.py
+++ dataToDatabaseTest1.py
@@ -7,6 +7,7 @@ contactsMain = "/var/www/html/contact.txt"
 database = "/root/disk3/database.db"
 table1 = 'table1'
 column1 = 'column1'
+column2 = 'column2'
 
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
@@ -17,7 +18,7 @@ with open(contactsMain, "r") as f:
 		print(line)
 		try:
 			c.execute("INSERT INTO {tn} {cn}VALUES (123456, 'test')".\
-				format(tn=table_name, idf=id_column, cn=column_name))
+				format(tn=table1, idf=column1, cn=column_name))
 		except s.IntegrityError:
 			print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
 		c.commit()
