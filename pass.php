59c41bd6ee408e162579f3fc13494d2417a67645
sschatz1997 <s.schatz1997@gmail.com>
2018-09-04 13:10:36 -0400

    hash verification

--- pass.php
+++ pass.php
@@ -9,6 +9,13 @@ $file = fopen("pass.txt", "a") or die ("file not open");
 $f = $username." , ".$hash;
 fputs($file,$f) or die ("no data was written");
 
+if (password_verify($password, $hash)) {
+	print "valid password";
+} else {
+	print "Password not valid contact admin for details"
+}
+
+
 fclose($file);
 header("Location: http://10.1.121.102/Redirect.html"); 
 exit();
