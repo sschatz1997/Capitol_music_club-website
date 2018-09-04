4aa744ac19e369b1aca9cc3d4eabe0ac3730c57d
sschatz1997 <s.schatz1997@gmail.com>
2018-09-04 14:23:45 -0400

    s

--- websiteUpdater.py
+++ websiteUpdater.py
@@ -7,7 +7,7 @@ sleep(2)
 os.system("cp viewcounter.php /var/www/html/viewcounter.php")
 sleep(0.5)
 
-os.system("cp index.html /var/www/index.html")
+os.system("cp index.html /var/www/html/index.html")
 sleep(0.5)
 
 os.system("cp styleSheet.css /var/www/html/styleSheet.css")
