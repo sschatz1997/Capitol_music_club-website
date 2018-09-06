1c19a7030a044931c84b409672a11bdffa48f62d
sschatz1997 <s.schatz1997@gmail.com>
2018-09-06 13:51:46 -0400

    update

--- pass.php
+++ pass.php
@@ -1,12 +1,14 @@
 <?php
 
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 $hash = password_hash($password, PASSWORD_BCRYPT);
 
 $file = fopen("pass.txt", "a") or die ("file not open");
 
-$f = $username." , ".$hash. "\n";
+$f = $first_name." , ".$last_name." , ".$username." , ".$hash. "\n";
 fputs($file,$f) or die ("no data was written");
 
 fclose($file);
