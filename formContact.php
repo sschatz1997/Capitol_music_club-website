538aad3eef2184ad7b83401a568eff5b863cae68
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 10:42:07 -0400

    update

--- formContact.php
+++ formContact.php
@@ -3,12 +3,19 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$file = fopen("contact.txt", "a") or die ("file not open");
+$file1 = fopen("contact.txt", "a") or die ("file not open");
 
 $f = $name." , ".$email;
-fputs($file,$f) or die ("no data was written");
+fputs($file1,$f) or die ("no data was written");
 
-fclose($file);
+fclose($file1);
 header("Location: http://10.1.121.102/Redirect.html"); 
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$file = "ip.txt";
+$content = file_get_contents($file);
+$content .= $ip . "\n";
+file_put_contents($file, $content);
+
 exit();
 ?>
