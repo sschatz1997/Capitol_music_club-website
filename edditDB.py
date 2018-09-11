00faafe87247e32327dc6febb929c9f5dcfa2a74
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 10:48:01 -0400

    add coulumn

---
+++ edditDB.py
@@ -0,0 +1,9 @@
+import sqlite3 as s
+from time import sleep
+database = "/root/disk3/database_main.sqlite"
+table1 = 'table1'
+column3 = 'ip'
+
+
+cur.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table1, cn=column3, ct=column_type))
