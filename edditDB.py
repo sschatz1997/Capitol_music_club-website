a41837cce3967d7e7726151504e843f64562c7ed
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 10:49:02 -0400

    fix

--- edditDB.py
+++ edditDB.py
@@ -3,7 +3,8 @@ from time import sleep
 database = "/root/disk3/database_main.sqlite"
 table1 = 'table1'
 column3 = 'ip'
-
+c = s.connect(database)
+cur = c.cursor()
 
 cur.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
         .format(tn=table1, cn=column3, ct=column_type))
