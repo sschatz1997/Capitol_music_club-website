7c8fefab13d33aed9e4f646165567035b03a6e2a
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 17:36:56 -0400

    test

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from multiprocessing import Pipe, Process, Array
-from main import program
+from main import main
 from time import sleep as s
 
 def main():
-	prog = program()
+	prog = main()
 	print("starting importing to databases")
 	prog.start()
 	s(45)
