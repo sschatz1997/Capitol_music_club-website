a409ecceaae517850dcf1bbfba61ca5a82da0c60
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 10:34:35 -0400

    fix

--- viewcounter.php
+++ viewcounter.php
@@ -1,9 +1,10 @@
 <?php
 
 /* counter */
+// opens countlog.txt to change new hit number
 
 //opens countlog.txt to read the number of hits
-$datei = fopen("countlog.txt","a");
+/*$datei = fopen("countlog.txt","a");
 $count = fgets($datei,1000);
 fclose($datei);
 $count=$count + 1 ;
@@ -11,15 +12,16 @@ echo "$count" ;
 echo " hits" ;
 echo "\n" ;
 
-// opens countlog.txt to change new hit number
 $datei = fopen("countlog.txt","w");
 fwrite($datei, $count);
-fclose($datei);
+fclose($datei);*/
 
-$file = fopen("ip.txt","a");
-$ip=$_SERVER['REMOTE_ADDR'];
-echo $ip;
-echo fwrite($file,$ip);
-fclose($file);
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$file = "ip.txt";
+$content = file_get_contents($file);
+$content .= $ip . "\n";
+file_put_contents($file, $content);
+
 
 ?>
