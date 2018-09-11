181144dbf13077ba62fa63b27fdd33bebdb8d954
sschatz1997 <s.schatz1997@gmail.com>
2018-09-11 17:55:30 -0400

    fix?

--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php
-    $dir = '/root/disk3/password_main.sqlite';
-    $dbh = new PDO($dir) or die ("cannot open");
-    $password = stripslashes($password);
-    $username = $_POST['username'];
-    $hash = password_verify[$password, PASSWORD_BCRYPT];
-	$password = $hash
-    $username = stripslashes($username);
-    $query = "SELECT * FROM password_main.sqlite WHERE username='$username' AND password='$password'";
-    $result = mysql_query($query);
-    $count = mysql_num_rows($result);
+$dir = '/root/disk3/password_main.sqlite';
+$dbh = new PDO($dir) or die ("cannot open");
+$password = stripslashes($password);
+$username = $_POST['username'];
+$hash = password_verify[$password, PASSWORD_BCRYPT];
+$password = $hash
+$username = stripslashes($username);
+$query = "SELECT * FROM password_main.sqlite WHERE username='$username' AND password='$password'";
+$result = mysql_query($query);
+$count = mysql_num_rows($result);
 
-    if($count==1){
-    echo'loged on';
-    }
-	
-	header("Location: http://10.1.121.102/Redirect.html"); 
-	exit();
+if($count==1){
+echo'loged on';
+}
+
+header("Location: http://10.1.121.102/Redirect.html"); 
+exit();
 ?>
